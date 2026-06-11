ff30894162df567a1312ba82f103a95d0b49db8b
Viscora-Game <viscoragames@gmail.com>
2026-06-11 23:02:54 +0300

    Revert Level 20 boss green acid projectile damage back to 1

--- js/boss.js
+++ js/boss.js
@@ -1341,7 +1341,7 @@ export class CyberBoss extends Boss {
             const dist = Math.sqrt(dx * dx + dy * dy);
             if (dist < player.radius + p.radius - 2) {
                 if (!player.isDead) {
-                    player.takeDamage(2);
+                    player.takeDamage(1);
                 }
                 this.projectiles.splice(i, 1);
                 continue;
